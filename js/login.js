308ea94ba1169441f057736dcfedd428a8c66d53
Nelte Vreeke <nvreeke@f1r4s11.codam.nl>
2019-07-19 15:27:58 +0200

    started on registerform functionalities

--- js/login.js
+++ js/login.js
@@ -1,4 +1,25 @@
-const getUsername = () => {
-    const elUsername = document.getElementByID("register-username");
-    console.log(elUsername.value);
-}
+const elRegister = document.getElementById('register');
+const elUsername = document.getElementById('register-username');
+const elEmail = document.getElementById('register-mail');
+const elPassword = document.getElementById('register-password');
+const elRepPassword = document.getElementById('register-reppassword');
+
+elRegister.onclick = () => {
+    if (elUsername.value === "") {
+        return;
+    }
+    if (elEmail.value === "") {
+        return;
+    }
+    if (elPassword.value === "") {
+        return;
+    }
+    if (elRepPassword.value === "") {
+        return;
+    }
+    if (elRepPassword.value !== elPassword.value) {
+        return;
+    }
+
+    const userData = Array();
+};
