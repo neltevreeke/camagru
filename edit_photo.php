5b197398ab838ee1bfa94368b4de27514f06d4e9
Nelte Vreeke <nvreeke@student.codam.nl>
2019-10-14 19:40:40 +0200

    Starting on mobile responsiveness

--- edit_photo.php
+++ edit_photo.php
@@ -2,6 +2,7 @@
     <head>
         <title>Create photo - Camagru</title>
         <link rel="stylesheet" type="text/css" href="style/style.css">
+        <meta name='viewport' content='width=device-width, minimum-scale=1, maximum-scale=1, user-scalable=no' />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
